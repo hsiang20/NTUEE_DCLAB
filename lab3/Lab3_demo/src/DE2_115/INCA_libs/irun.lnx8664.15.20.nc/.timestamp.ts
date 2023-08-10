81363d3389adf7e2696e0fe8edbce82623be5473
hsiang20 <h511168@nehs.hc.edu.tw>
2023-08-10 15:11:43 +0800

    directory structure modified

--- lab3/Lab3_demo/src/DE2_115/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
+++
@@ -1,1 +0,0 @@
-1636995700 /home/team07/lab3/src/DE2_115/DE2_115.sv
--- lab3/src/tb/INCA_libs/irun.lnx8664.15.20.nc/files.ts
+++
@@ -1,4 +0,0 @@
-1636943676 /home/team07/lab3/src/I2cInitializer.sv
-1637225517 /home/team07/lab3/src/tb/I2CInitializer_tb.sv
-1637227414 /home/team07/lab3/src/tb/AudRecorder_tb.sv
-1637168624 /home/team07/lab3/src/AudRecorder.sv
